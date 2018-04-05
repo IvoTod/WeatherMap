6c8bf48b696eb3c0d74c182b6f32892f4c02fc7c
IvoTod <ivo.tod.todorov@gmail.com>
2018-04-05 06:58:10 +0300

    Link frontend with backend

--- Front-end/script.js
+++ Front-end/script.js
@@ -1,69 +1,102 @@
 function showTemperature() {
     $("#pollution-button").removeClass("clicked");
     $("#temperature-button").addClass("clicked");
-    $("#airPressure").hide();
-    $("#apm").hide();
-    $("#temperature").show();
-    $("#humidity").show();
+    $(".pollution").hide();
+    $(".temperature").show();
 }
 
 function showPollution() {
     $("#temperature-button").removeClass("clicked");
     $("#pollution-button").addClass("clicked");
-    $("#temperature").hide();
-    $("#humidity").hide();
-    $("#airPressure").show();
-    $("#apm").show();
+    $(".temperature").hide();
+    $(".pollution").show();
 }
 
 function initMap() {
-        var uluru = {lat: -37.663712, lng: 144.844788, name: "Melbourne"};
-        var marker2 = {lat: 36.114647, lng: -115.172813, name: "Las Vegas"};
-        var marker3 = {lat: 52.520008, lng: 13.404954, name: "Berlin"};
-        var marker4 = {lat: 51.509865, lng: -0.118092, name: "London"};
-        var marker5 = {lat: 47.751076, lng: -120.740135, name: "Washington"};
-        var marker6 = {lat: 55.751244, lng: 37.618423, name: "Moscow"};
-        var marker7 = {lat: -29.851847, lng: 30.993368, name: "South Africa"};
-        var locations = [];
-        locations.push(uluru);
-        locations.push(marker2);
-        locations.push(marker3);
-        locations.push(marker4);
-        locations.push(marker5);
-        locations.push(marker6);
-        locations.push(marker7);
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 3,
-          center: uluru
-        });
-        for (var i = 0; i < locations.length; i++) {
-            var marker = new google.maps.Marker({
-                animation: google.maps.Animation.DROP,
-                position: locations[i],
-                map: map,
-                id: i,
-                name: locations[i].name 
-            });
-            google.maps.event.addListener(marker, 'click', function() {
-                $("#name").text(this.name);
-                $.ajax({
-                    type: 'GET',
-                    url: "/WeatherMap/url/Get/average?id=" + this.id,
-                    success: function (data) {
-                        for (var i = 0; i < data.length; i++) {
-                            $("#hour").append("<br />" + data[i].hour);
-                            $("#airPressure").append("<br />" + data[i].airPressure);
-                            $("#temperature").append("<br />" + data[i].temperature);
-                            $("#humidity").append("<br />" + data[i].humidity);
-                            $("#apm").append("<br />" + data[i].apm);
-                        }
-                        $("#hour").empty();
-                        $("#airPressure").empty();
-                        $("#temperature").empty();
-                        $("#humidity").empty();
-                        $("#apm").empty();
-                    }
-                });
-            });
-        }
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 3,
+      center: {lat: 0, lng: 0}
+    });
+    var locations = [];
+    $.ajax({type:"GET", 
+	    crossDomain: true, 
+	    url:"http://localhost:8080/WeatherMap/url/Get/all", 
+	    contentType: "application/x-www-form-urlencoded; charset=utf-8", 
+	    success: function( locations ) {
+		    for (var i = 0; i < locations.length; i++) {
+			    var marker = new google.maps.Marker({
+				    animation: google.maps.Animation.DROP,
+				    position: {'lat': locations[i].latitude, 'lng': locations[i].longitude},
+				    map: map,
+				    id: locations[i].id,
+				    name: locations[i].name 
+			    });
+			    google.maps.event.addListener(marker, 'click', function(test) { 
+				    $("#name").text(this.name);
+				    $(".field").text("");
+				    $.ajax({type:"GET", 
+					    crossDomain: true, 
+					    url:"http://localhost:8080/WeatherMap/url/Get/station?id=" + this.id, 
+					    contentType: "application/x-www-form-urlencoded; charset=utf-8", 
+					    success: function( data ) {
+						    console.log(data);
+						    $(".temperatureCurrent span").text(data.temperature);
+						    $(".humidityCurrent span").text(data.humidity);
+						    $(".apmCurrent span").text(data.apm);
+						    $(".airPressureCurrent span").text(data.airPressure);
+					    },
+					    error: function() {
+						    console.log("error");
+					    }
+				    });
+				    $.ajax({
+					type: 'GET',
+					url: "http://localhost:8080/WeatherMap/url/Get/average?id=" + this.id,
+					success: function (data) {
+					    $("#2h,#5h,#12h").hide();
+					    console.log(data);
+					    if(data["2hr"]) {
+						$("#2h").show();
+						$(".airPressure2H span").text(data["2hr"].airPressure);
+						$(".temperature2H span").text(data["2hr"].temperature);
+						$(".humidity2H span").text(data["2hr"].humidity);
+						$(".apm2H span").text(data["2hr"].apm);
+					    }
+					    if(data["5hr"]) {
+						$("#5h").show();
+						$(".airPressure5H span").text(data["5hr"].airPressure);
+						$(".temperature5H span").text(data["5hr"].temperature);
+						$(".humidity5H span").text(data["5hr"].humidity);
+						$(".apm5H span").text(data["5hr"].apm);
+					    }
+					    if(data["12hr"]) {
+						$("#12h").show();
+						$(".airPressure12H span").text(data["12hr"].airPressure);
+						$(".temperature12H span").text(data["12hr"].temperature);
+						$(".humidity12H span").text(data["12hr"].humidity);
+						$(".apm12H span").text(data["12hr"].apm);
+					    }
+					}
+				    });
+			    });
+		    }
+	    },
+	    error: function() {
+		    console.log("error");
+	    }
+    });
+    //for (var i = 0; i < locations.length; i++) {
+	//var marker = new google.maps.Marker({
+	    //animation: google.maps.Animation.DROP,
+	    //position: locations[i],
+	    //map: map,
+	    //id: i,
+	    //name: locations[i].name 
+	//});
+	//google.maps.event.addListener(marker, 'click', function() {
+	//});
+    //}
 }
+$(document).ready(function() {
+    showTemperature();
+});
